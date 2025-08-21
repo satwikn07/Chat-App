ef1647e21af9797410ffe32de14aaa3de4f2dd65
satwikn07 <satwikn07@gmail.com>
2025-08-21 12:34:17 +0530

    add members, remove members and leave a group chat

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,7 +1,7 @@
 // routes/chatRoutes.js
 const express = require('express');
 const router = express.Router();
-const {createChat, getChatDetails, getUserChats, renameGroupChat} = require('../controllers/chatController');
+const {createChat, getChatDetails, getUserChats, renameGroupChat, removeFromGroup, addToGroup, leaveGroup } = require('../controllers/chatController');
 const authMiddleware = require('../utils/authMiddleware');
 // const {
 //   getChatDetails,
@@ -13,5 +13,8 @@ router.post('/create', authMiddleware, createChat);
 router.get('/:chatId', authMiddleware, getChatDetails);
 router.get('/user/chats', authMiddleware, getUserChats);
 router.post('/:chatId/rename', authMiddleware, renameGroupChat);
+router.delete('/:userId/:chatId/remove', authMiddleware, removeFromGroup);
+router.post('/:userId/:chatId/add', authMiddleware, addToGroup);
+router.post('/:chatId/leave', authMiddleware, leaveGroup);
 
 module.exports = router;
