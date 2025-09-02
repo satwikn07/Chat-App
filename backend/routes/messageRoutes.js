de5d11f9a0674433ba6b37c3fe5926e2d6778108
satwikn07 <satwikn07@gmail.com>
2025-09-02 11:54:53 +0530

    soft and hard delete messages

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { fetchMessages } = require('../controllers/messageController');
+const { fetchMessages, deleteMessage } = require('../controllers/messageController');
 const authMiddleware = require('../utils/authMiddleware');
+const { route } = require('./authRoutes');
 
 // router.post("/:chatId/message", authMiddleware, sendMessage);
-router.get("/:chatId/messages", authMiddleware, fetchMessages);
-
+router.get("/:chatId/fetchAllMessages", authMiddleware, fetchMessages);
+router.delete("/:chatId/:messageId/delete", authMiddleware, deleteMessage);
 module.exports = router;
